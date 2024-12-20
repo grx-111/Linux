8e6ff640a3d1c68eaeaf26db2adcdec2367050d0
grx-111 <2856650047@qq.com>
2024-12-20 15:44:02 +0800

    Initial commit

---
+++ linux-springboot/src/main/java/com/example/LinuxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxSpringbootApplication.class, args);
+    }
+
+}
